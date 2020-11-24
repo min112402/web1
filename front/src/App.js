b4708d6e97eb15bb60e37f37d864b3b6e054f6d9
CSE1JIRm9 <54717129+CSE1JIRm9@users.noreply.github.com>
2020-11-25 03:11:14 +0900

    201125

--- front/src/App.js
+++ front/src/App.js
@@ -1,21 +1,25 @@
 import React, {Fragment} from 'react';
 import './App.css';
 import {BrowserRouter, Route, Switch } from "react-router-dom"
+import { Helmet } from 'react-helmet'
 
 import Portfolio from "./views/Portfolio/Portfolio"
 import LandingPage from './views/LandingPage/LandingPage';
-import Contact from './views/Contact/Contact';
 import Shop from './views/Shop/Shop';
 
 import Nav from './views/Nav/Nav'
+const TITLE = "KWON_theFACT0RY"
 
 class App extends React.Component {
 
-  
   render(){
+
     return (
     <BrowserRouter >
       <div className="wrapper">
+        <Helmet>
+          <title>{TITLE}</title>
+        </Helmet>
         <Nav />
         <Fragment>
           <div class="contentContainer">
@@ -23,13 +27,12 @@ class App extends React.Component {
               <Route exact path="/" component={LandingPage} />
               <Route path="/shop" component={Shop} />
               <Route path="/portfolio" component={Portfolio} />
-              <Route path="/contact" component={Contact} />
             </Switch>
           </div>
         </Fragment>
       </div>
-    </BrowserRouter>    
-    
+    </BrowserRouter>
+
   );
 }
 }
