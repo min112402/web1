e40c69a59547e494b690ac8265746d9a67bdafe9
Ubuntu <ubuntu@ip-172-31-35-110.ap-northeast-2.compute.internal>
2021-01-14 13:40:29 +0000

    settings for deployment

--- back/factory/factory/urls.py
+++ back/factory/factory/urls.py
@@ -18,9 +18,10 @@ from django.urls import path,include
 
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic import TemplateView
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('api.urls')),
+    path('api/', include('api.urls')), 
 ]
 
 
