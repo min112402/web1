431bd1e4b35977f95e98e389f6e76a5925ed0498
sangmin <min112402@gmail.com>
2021-01-25 15:29:45 +0900

    django setting 변경

--- back/factory/factory/settings.py
+++ back/factory/factory/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'dokydrvnph&lu0j!db^pwp^i8yb)t-q=nhyl+iv=$ompz4i=iy'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
